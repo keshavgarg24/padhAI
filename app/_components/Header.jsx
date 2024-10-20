c7a57aa4cf090a267f6fb4c5d57ce1cefd14dbbc
Keshav Garg <141502522+keshavgarg24@users.noreply.github.com>
2024-10-20 23:33:02 +0530

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -5,21 +5,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <>
-    <div className="flex justify-between p-5 shadow-sm items-center">
-      <div className="flex items-center gap-2">
-      </div>
+    <div className="relative z-50 flex justify-between p-5  items-center">
+      <div className="flex items-center gap-2"></div>
       <div className="flex justify-center items-center gap-10 cursor-pointer">
-       <Link href={"https://github.com/keshavgarg24/padhAI" }>
-         <RxGithubLogo className="text-3xl"/>
-       </Link>          
-      <Link href={'/courses'} >
-
-      <Button variant="startButton" > Sign in</Button>
-      </Link>
+        <Link href={"https://github.com/keshavgarg24/padhAI"}>
+          <RxGithubLogo className="text-3xl" />
+        </Link>
+        <Link href={'/courses'}>
+          <Button variant="startButton">Get started</Button>
+        </Link>
       </div>
     </div>
-    </>
   );
 };
 
