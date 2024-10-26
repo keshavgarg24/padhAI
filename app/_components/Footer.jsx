168767abe554f31acf5654b699e0077661b7abd5
Keshav Garg <141502522+keshavgarg24@users.noreply.github.com>
2024-10-26 11:42:10 +0530

    minor navigation

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,8 +1,8 @@
-"use client"
-import { AnimatedTooltipPreview } from '@/components/team'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { AnimatedTooltipPreview } from '@/components/team';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 
 const Footer = () => {
   const path = usePathname();
@@ -10,29 +10,28 @@ const Footer = () => {
     {
       id: 1,
       name: 'About Us',
-      path: '/about'
+      path: '/about',
     },
     {
       id: 2,
       name: 'Contact Us',
-      path: '/contact'
+      path: '/contact',
     },
     {
       id: 3,
       name: 'Terms & Conditions',
-      path: '/terms'
+      path: '/terms',
     },
-  ]
-  
+  ];
+
   return (
     <footer className="bg-white dark:bg-gray-900 py-8">
-       {/* Meet the TEAM Section */}
-       <div className="flex flex-col items-center text-center z-10 mt-10 mb-12">
-          <h2 className="text-3xl font-bold text-blue-500 mb-6">Meet the TEAM</h2>
-          <AnimatedTooltipPreview />
-        </div>
+      {/* Meet the TEAM Section */}
+      <div className="flex flex-col items-center text-center z-10 mt-10 mb-12">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6">Meet the TEAM</h2>
+        <AnimatedTooltipPreview />
+      </div>
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        
         {/* Footer Content */}
         <div className="text-center">
           <ul className="mt-10 flex justify-center gap-12 text-xl font-medium text-gray-500 dark:text-gray-400">
@@ -50,11 +49,9 @@ const Footer = () => {
             &copy; {new Date().getFullYear()} padhAI. All Rights Reserved.
           </p>
         </div>
-        
       </div>
-      
     </footer>
   );
-}
+};
 
 export default Footer;
